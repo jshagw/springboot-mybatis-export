46453691c6738cc36652a82b303c461e35cc8718
gaowei <13857120590@qq.com>
2019-08-11 14:31:38 +0800

    excel to pdf

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.jodconverter.DocumentConverter;
+import org.jodconverter.office.OfficeException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    @Autowired
+    private DocumentConverter documentConverter;
 
 //    @Autowired
 //    private TestUserMapper testUserMapper;
@@ -18,4 +25,12 @@ public class DemoApplicationTests {
 //        System.out.println("L1= " + user.getL1());
     }
 
+    @Test
+    public void testExcel2pdf() throws OfficeException {
+        excel2pdf("F:\\temp\\office\\test.xlsx", "F:\\temp\\office\\test.pdf");
+    }
+
+    private void excel2pdf(String src, String dst) throws OfficeException {
+        documentConverter.convert(new File(src)).to(new File(dst)).execute();
+    }
 }
